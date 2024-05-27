ca21efc16060b6c736e434df3655e963f3ede516
KevinAlpizarDev <kevinalpizardev@gmail.com>
2024-05-27 12:50:15 -0600

    Change extreme

--- dist/script.js
+++ dist/script.js
@@ -9,23 +9,31 @@ let thirdInput = document.getElementById("third-input");
 let firstSpan = document.getElementById("first-span");
 let secondSpan = document.getElementById("second-span");
 let thirdSpan = document.getElementById("third-span");
-let width1 = 0;
-let firsInputValue = 0;
+let firsInputValue;
 let mainValue = 0;
-firsInput.addEventListener('change', (event) => {
-    firsInputValue = event.target.value;
-    function task1(id, subTaskCompleted1, subTaskToComplete1) {
-        firsInputValue = (subTaskCompleted1 / subTaskToComplete1 * 100);
-        return id.style.width = firsInputValue + "%";
-    }
-    task1(firstBar, Number(firsInputValue), 6);
-});
-let thirdInputValue = 0;
-thirdInput.addEventListener('change', (event) => {
-    thirdInputValue = event.target.value;
-    function task3(subTaskCompleted1, subTaskToComplete1) {
-        thirdInputValue = (subTaskCompleted1 / subTaskToComplete1 * 100);
-        return thirdBar.style.width = thirdInputValue + "%";
-    }
-    task3(Number(thirdInputValue), 3);
-});
+let inputValue;
+let subTaskCompleted;
+let totalTasks = 0;
+const vroadTaskProgress = (wichInput, subTaskToComplet, nameBar) => {
+    totalTasks++;
+    wichInput.addEventListener("change", (event) => {
+        let inputValue = event.target.value;
+        const subTaskCompleted = Math.round(Number(inputValue));
+        const ProgressBar = (id, subTaskCompleted, subTaskToComplet) => {
+            let porcentOfprogress;
+            if (subTaskCompleted <= subTaskToComplet) {
+                porcentOfprogress = (subTaskCompleted / subTaskToComplet) * 100;
+                id.style.width = porcentOfprogress + "%";
+                let meta = (subTaskCompleted / 100 * totalTasks) / 100;
+                console.log(meta);
+            }
+            else {
+                porcentOfprogress = 100;
+            }
+        };
+        ProgressBar(nameBar, subTaskCompleted, subTaskToComplet);
+    });
+};
+vroadTaskProgress(firsInput, 5, firstBar);
+vroadTaskProgress(secondInput, 10, secondBar);
+vroadTaskProgress(thirdInput, 15, thirdBar);
--- src/script.ts
+++ src/script.ts
@@ -1,60 +1,190 @@
-
 // PROGRESS VARIABLES
 let mainBar = document.getElementById("main-bar")
 let firstBar = document.getElementById("first-bar")!
-let secondBar = document.getElementById("second-bar")
-let thirdBar = document.getElementById("third-bar")
+let secondBar = document.getElementById("second-bar")!
+let thirdBar = document.getElementById("third-bar")!
 // INPUTS VARIABLES
-
 let firsInput = document.getElementById("first-input")
 let secondInput = document.getElementById("second-input")
 let thirdInput = document.getElementById("third-input")
 // SPAN VARIABLES
-let firstSpan: HTMLElement | null = document.getElementById("first-span")
+let firstSpan= document.getElementById("first-span")
 let secondSpan = document.getElementById("second-span")
 let thirdSpan = document.getElementById("third-span")
-
 // FUNCION 1
-let firsInputValue: number | string = 0
+let firsInputValue: number | string 
 let mainValue: number | string = 0
+// ejecution()
+let inputValue: string
+///////////////////////////////////////////////////
+let subTaskCompleted: number;
+let totalTasks = 0
+let otraVariable = 0
+const vroadTaskProgress = (
+  wichInput: HTMLElement | null,
+  subTaskToComplet: number,
+  nameBar: HTMLElement
+) => {
+  totalTasks++
+  wichInput!.addEventListener("change", (event) => {
+    let inputValue = (event.target as HTMLInputElement).value;
+    const subTaskCompleted = Math.round(Number(inputValue)); // Round the value
+    // console.log(wichInput?.id);
+    // console.log(inputValue.length);
+    const ProgressBar = (id: HTMLElement, subTaskCompleted: number, subTaskToComplet: number) => {
+      let porcentOfprogress;
+      // if (subTaskCompleted <= subTaskToComplet) {
+        if (subTaskCompleted <= subTaskToComplet) {
+        porcentOfprogress = (subTaskCompleted / subTaskToComplet) * 100;
+        // console.log(porcentOfprogress);
+        id.style.width = porcentOfprogress + "%";
+        // console.log(subTaskCompleted);
+        ///////////////////////////////////////
 
-firsInput!.addEventListener('change', (event) => {
-  firsInputValue = (event.target as HTMLInputElement).value
-  // console.log(firsInputValue.value) // No error because 'value' exists on 'HTMLInputElement'.
-  function task1(id: HTMLElement, subTaskCompleted1: number, subTaskToComplete1: number) {
-    firsInputValue = (subTaskCompleted1 / subTaskToComplete1 * 100) // Las subtareas realizadas, entre  el numero de subtareas existentes
-    return id!.style.width = firsInputValue + "%"
-  }
-  task1(firstBar, Number(firsInputValue), 6)
-  // console.log(firsInputValue)
-})
+
+ 
+      
+      //////////////////////////////////////
+      } else {
+        porcentOfprogress = 100; // Limit the progress to 100%
+        // Disable the input when progress reaches 100
+      }
+    };
 
 
 
-// let secondInputValue: number | string = 0
-// // firsInput.addEventListener("input");
-// secondInput!.addEventListener('change', (event) => {
-//   secondInputValue = (event.target as HTMLInputElement).value
+    ProgressBar(nameBar, subTaskCompleted, subTaskToComplet);
+  });
+  // console.log("Cantidad total de tareas actuales:", totalTasks);
+};
+// Llama a vroadTaskProgress para configurar el event listener
+// console.log("Cantidad total de tareas actuales:", totalTasks);
+vroadTaskProgress(firsInput, 5, firstBar);
+vroadTaskProgress(secondInput, 10, secondBar);
+ vroadTaskProgress(thirdInput, 15, thirdBar);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // PROGRESS VARIABLES
+// let mainBar = document.getElementById("main-bar")
+// let firstBar = document.getElementById("first-bar")!
+// let secondBar = document.getElementById("second-bar")!
+// let thirdBar = document.getElementById("third-bar")!
+// // INPUTS VARIABLES
+// let firsInput = document.getElementById("first-input")
+// let secondInput = document.getElementById("second-input")
+// let thirdInput = document.getElementById("third-input")
+// // SPAN VARIABLES
+// let firstSpan= document.getElementById("first-span")
+// let secondSpan = document.getElementById("second-span")
+// let thirdSpan = document.getElementById("third-span")
+// // FUNCION 1
+// let firsInputValue: number | string 
+// let mainValue: number | string = 0
+// // ejecution()
+// let inputValue: string
+// ///////////////////////////////////////////////////
+// let subTaskCompleted: number;
+// let totalTasks = 0
+// const vroadTaskProgress = (
+//   wichInput: HTMLElement | null,
+//   subTaskToComplet: number,
+//   nameBar: HTMLElement
+// ) => {
+//   totalTasks++
+//   wichInput!.addEventListener("change", (event) => {
+//     let inputValue = (event.target as HTMLInputElement).value;
+//     const subTaskCompleted = Math.round(Number(inputValue)); // Round the value
+//     console.log(wichInput?.id);
+//     console.log(inputValue.length);
+//     const ProgressBar = (id: HTMLElement, subTaskCompleted: number, subTaskToComplet: number) => {
+//       let porcentOfprogress;
+//       // if (subTaskCompleted <= subTaskToComplet) {
+//         if (subTaskCompleted <= subTaskToComplet) {
+//         porcentOfprogress = (subTaskCompleted / subTaskToComplet) * 100;
+//         console.log(porcentOfprogress);
+//         id.style.width = porcentOfprogress + "%";
+//         // console.log(subTaskCompleted);
+//       } else {
+//         porcentOfprogress = 100; // Limit the progress to 100%
+//         // Disable the input when progress reaches 100
+//       }
+//     };
+//     ProgressBar(nameBar, subTaskCompleted, subTaskToComplet);
+//   });
+//   console.log("Cantidad total de tareas actuales:", totalTasks);
+// };
+// // Llama a vroadTaskProgress para configurar el event listener
+// // console.log("Cantidad total de tareas actuales:", totalTasks);
+// vroadTaskProgress(firsInput, 5, firstBar);
+// vroadTaskProgress(secondInput, 10, secondBar);
+//  vroadTaskProgress(thirdInput, 15, thirdBar);
+
+
+
+
+
+// // PROGRESS VARIABLES
+// let mainBar = document.getElementById("main-bar")
+// let firstBar = document.getElementById("first-bar")!
+// let secondBar = document.getElementById("second-bar")
+// let thirdBar = document.getElementById("third-bar")
+// // INPUTS VARIABLES
+
+// let firsInput = document.getElementById("first-input")
+// let secondInput = document.getElementById("second-input")
+// let thirdInput = document.getElementById("third-input")
+// // SPAN VARIABLES
+// let firstSpan: HTMLElement | null = document.getElementById("first-span")
+// let secondSpan = document.getElementById("second-span")
+// let thirdSpan = document.getElementById("third-span")
+
+// // FUNCION 1
+// let firsInputValue: number | string = 0
+// let mainValue: number | string = 0
+
+// firsInput!.addEventListener('change', (event) => {
+//   firsInputValue = (event.target as HTMLInputElement).value
 //   // console.log(firsInputValue.value) // No error because 'value' exists on 'HTMLInputElement'.
-//   function task2(id: HTMLElement, subTaskCompleted1: number, subTaskToComplete1: number) {
-//     secondInputValue = (subTaskCompleted1 / subTaskToComplete1 * 100) // Las subtareas realizadas, entre  el numero de subtareas existentes
-//     return id!.style.width = secondInputValue + "%"
-//   } task2(first-bar, Number(secondInputValue), 2)
+//   function task1(id: HTMLElement, subTaskCompleted1: number, subTaskToComplete1: number) {
+//     firsInputValue = (subTaskCompleted1 / subTaskToComplete1 * 100) // Las subtareas realizadas, entre  el numero de subtareas existentes
+//     return id!.style.width = firsInputValue + "%"
+//   }
+//   task1(firstBar, Number(firsInputValue), 100)
+//   // console.log(firsInputValue)
 // })
 
 
 
-
-
-
-
-// FUNCION HELPER
-// function helper(subTaskCompleted1: number, subTaskToComplete1: number) {
-//   mainValue = (subTaskCompleted1 / subTaskToComplete1 * 100) // Las subtareas realizadas, entre  el numero de subtareas existentes
-//   return mainBar!.style.width = mainValue + "%"
-// }
-// console.log(helper(Number(firsInputValue), 6));
-// // // FUNCION 2
 // let secondInputValue: number | string = 0
 // // firsInput.addEventListener("input");
 // secondInput!.addEventListener('change', (event) => {
@@ -63,19 +193,37 @@ firsInput!.addEventListener('change', (event) => {
 //   function task2(subTaskCompleted1: number, subTaskToComplete1: number) {
 //     secondInputValue = (subTaskCompleted1 / subTaskToComplete1 * 100) // Las subtareas realizadas, entre  el numero de subtareas existentes
 //     return secondBar!.style.width = secondInputValue + "%"
-//   } task2(Number(secondInputValue), 2)
+//   } task2( Number(secondInputValue), 2)
 // })
-// // FUNCION 3
-let thirdInputValue: number | string = 0
-// firsInput.addEventListener("input");
-thirdInput!.addEventListener('change', (event) => {
-  thirdInputValue = (event.target as HTMLInputElement).value
-  // console.log(firsInputValue.value) // No error because 'value' exists on 'HTMLInputElement'.
-  function task3(subTaskCompleted1: number, subTaskToComplete1: number) {
-    thirdInputValue = (subTaskCompleted1 / subTaskToComplete1 * 100) // Las subtareas realizadas, entre  el numero de subtareas existentes
-    return thirdBar!.style.width = thirdInputValue + "%"
-  } task3(Number(thirdInputValue), 3)
-})
+
+
+
+
+// let secondInputValue: number | string = 0
+// // firsInput.addEventListener("input");
+// secondInput!.addEventListener('change', (event) => {
+//   secondInputValue = (event.target as HTMLInputElement).value
+//   // console.log(firsInputValue.value) // No error because 'value' exists on 'HTMLInputElement'.
+//   function task2(subTaskCompleted1: number, subTaskToComplete1: number) {
+//     secondInputValue = (subTaskCompleted1 / subTaskToComplete1 * 100) // Las subtareas realizadas, entre  el numero de subtareas existentes
+//     return secondBar!.style.width = secondInputValue + "%"
+//   } task2( Number(secondInputValue), 2)
+// })
+
+
+
+
+// // // FUNCION 3
+// let thirdInputValue: number | string = 0
+// // firsInput.addEventListener("input");
+// thirdInput!.addEventListener('change', (event) => {
+//   thirdInputValue = (event.target as HTMLInputElement).value
+//   // console.log(firsInputValue.value) // No error because 'value' exists on 'HTMLInputElement'.
+//   function task3(subTaskCompleted1: number, subTaskToComplete1: number) {
+//     thirdInputValue = (subTaskCompleted1 / subTaskToComplete1 * 100) // Las subtareas realizadas, entre  el numero de subtareas existentes
+//     return thirdBar!.style.width = thirdInputValue + "%"
+//   } task3(Number(thirdInputValue), 3)
+// })
 
 
 // let mainValue: number | string = 0;
